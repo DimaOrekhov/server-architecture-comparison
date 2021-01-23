55cb67faec781b6645799afa775272e5fd1acff7
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2021-01-24 01:37:05 +0300

    Proper termination for sync blocking server

--- Server/src/main/kotlin/ru/itmo/java/architectures/server/synchronous/blocking/SynchronousBlockingServer.kt
+++ Server/src/main/kotlin/ru/itmo/java/architectures/server/synchronous/blocking/SynchronousBlockingServer.kt
@@ -2,24 +2,22 @@ package ru.itmo.java.architectures.server.synchronous.blocking
 
 import ru.itmo.java.architectures.common.Constants
 import ru.itmo.java.architectures.server.domain.TimedServer
-import java.net.InetAddress
+import java.net.InetSocketAddress
 import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.Executors
 
 class SynchronousBlockingServer(poolSize: Int) : TimedServer() {
 
+    private val serverSocket = ServerSocket()
     private val acceptPool = Executors.newSingleThreadExecutor()
     private val globalThreadPool = Executors.newFixedThreadPool(poolSize)
     override val clients: MutableCollection<SynchronousBlockingClientWorker> = mutableListOf()
 
     override fun start() {
         acceptPool.submit {
-            ServerSocket(
-                Constants.SERVER_PORT,
-                Constants.SERVER_BACKLOG,
-                InetAddress.getByName(Constants.SERVER_ADDRESS)
-            ).use { serverSocket ->
+            serverSocket.bind(InetSocketAddress(Constants.SERVER_ADDRESS, Constants.SERVER_PORT))
+            serverSocket.use { serverSocket ->
                 while (!Thread.interrupted()) {
                     val socket = serverSocket.accept()
                     createClient(socket)
@@ -29,6 +27,7 @@ class SynchronousBlockingServer(poolSize: Int) : TimedServer() {
     }
 
     override fun shutdown() {
+        serverSocket.close()
         acceptPool.shutdown()
         clients.forEach { it.shutdown() }
         globalThreadPool.shutdown()
